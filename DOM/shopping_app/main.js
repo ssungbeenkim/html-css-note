fef490be51ccebd0fff028ba4b31e0c791387a6f
wandakim <nwd1583@naver.com>
2023-03-02 01:13:51 +0900

    JS:upgrade

--- DOM/shopping_app/main.js
+++ DOM/shopping_app/main.js
@@ -1,6 +1,5 @@
 const addBtn = document.querySelector('.footer__button');
 const input = document.querySelector('.footer__input');
-const deleteBtn = document.querySelector('.item__delete');
 const items = document.querySelector('.items');
 /* 
 1. 입력창 엔터 눌리면 리스트 추가하고 초기화 포커스 
@@ -20,40 +19,37 @@ function onAdd() {
   input.value = '';
   input.focus();
 }
-
+let id = 0; //UUID
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const itemName = document.createElement('span');
-  itemName.setAttribute('class', 'item__name');
-  itemName.textContent = text;
-
-  const itemDelete = document.createElement('button');
-  itemDelete.setAttribute('class', 'item__delete');
-  itemDelete.innerHTML = '<i class="fa-sharp fa-solid fa-trash-can"></i>';
-  itemDelete.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-
-  const devider = document.createElement('div');
-  devider.setAttribute('class', 'devider');
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(devider);
-  item.appendChild(itemName);
-  item.appendChild(itemDelete);
-
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+  <div class="item">
+    <span class="item__name">${text}</span>
+    <button class="item__delete">
+      <i class="fa-sharp fa-solid fa-trash-can" data-id=${id}></i>
+    </button></div>
+  <div class="devider"></div>
+  `;
+  id++;
   return itemRow;
 }
 
-addBtn.addEventListener('click', onAdd);
+addBtn.addEventListener('click', () => onAdd());
 
 input.addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
     onAdd();
   }
 });
+
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id && event.target.tagName === 'I') {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
+});
+
+window.addEventListener('load', input.focus());
